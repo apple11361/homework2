377becd23e71e00cbd310fedb0daa45c2fc98be6
apple <apple113611361@gmail.com>
2016-03-21 11:47:46 +0800

    astyle

--- math-toolkit2.h
+++ math-toolkit2.h
@@ -25,34 +25,34 @@ double length(const double *v)
 static inline
 void add_vector(const double *a, const double *b, double *out)
 {
-        out[0] = a[0] + b[0];
-        out[1] = a[1] + b[1];
-        out[2] = a[2] + b[2];
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
 
 }
 
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-        out[0] = a[0] - b[0];
-        out[1] = a[1] - b[1];
-        out[2] = a[2] - b[2];
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
 }
 
 static inline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
-        out[0] = a[0] * b[0];
-        out[1] = a[1] * b[1];
-        out[2] = a[2] * b[2];
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
 }
 
 static inline
 void multiply_vector(const double *a, double b, double *out)
 {
-        out[0] = a[0] * b;
-	out[1] = a[1] * b;
-	out[2] = a[2] * b;
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
 }
 
 static inline
@@ -67,9 +67,9 @@ static inline
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
-        dp += v1[0] * v2[0];
-	dp += v1[1] * v2[1];
-	dp += v1[2] * v2[2];
+    dp += v1[0] * v2[0];
+    dp += v1[1] * v2[1];
+    dp += v1[2] * v2[2];
     return dp;
 }
 
